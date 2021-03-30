ed9ea27916818a90a7b3aea0ad11d5cfcc7b26ba
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-30 19:22:01 -0300

    add find

--- bloco_8/dia-2/aulas.js
+++ bloco_8/dia-2/aulas.js
@@ -16,11 +16,27 @@
 
 // numbers.forEach(multipliesFor2);
 
-const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+// const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
 
-const convertToUpperCase = (name, index) => {
-  names[index] = name.toUpperCase();
-};
+// const convertToUpperCase = (name, index) => {
+//   names[index] = name.toUpperCase();
+// };
 
-names.forEach(convertToUpperCase);
-console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+// names.forEach(convertToUpperCase);
+// console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.find(verifyEven);
+
+console.log(isEven); // 30
+
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
+
+console.log(isEven2); // 30
