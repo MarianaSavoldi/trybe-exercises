21893b18e938e3c63097a4de7904c438c6254525
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-30 19:10:31 -0300

    add forEach exemple 2

--- bloco_8/dia-2/aulas.js
+++ bloco_8/dia-2/aulas.js
@@ -8,10 +8,19 @@
 // Cada elemento do array: 0.25
 // Cada elemento do array: { comida: 'Chocolate' }
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const multipliesFor2 = (element) => {
-  console.log(`${element} * 2: ${element * 2}`);
+// const multipliesFor2 = (element) => {
+//   console.log(`${element} * 2: ${element * 2}`);
+// };
+
+// numbers.forEach(multipliesFor2);
+
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
 };
 
-numbers.forEach(multipliesFor2);
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
