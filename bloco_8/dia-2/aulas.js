2f6866a01f8058157f820f7c9c6fdb2da4c53e3a
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-30 19:31:59 -0300

    add every

--- bloco_8/dia-2/aulas.js
+++ bloco_8/dia-2/aulas.js
@@ -25,18 +25,25 @@
 // names.forEach(convertToUpperCase);
 // console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
 
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const verifyEven = (number) => number % 2 === 0;
+// const verifyEven = (number) => number % 2 === 0;
 
-const isEven = numbers.find(verifyEven);
+// const isEven = numbers.find(verifyEven);
 
-console.log(isEven); // 30
+// console.log(isEven); // 30
 
-console.log(verifyEven(9)); // False
-console.log(verifyEven(14)); // True
+// console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
 
-// Outra forma de ser realizada sem a necessidade de criar uma nova função.
-const isEven2 = numbers.find((number) => number % 2 === 0);
+// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
+// const isEven2 = numbers.find((number) => number % 2 === 0);
 
-console.log(isEven2); // 30
+// console.log(isEven2); // 30
+
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('x', listNames)); // false
