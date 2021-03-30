41691b9056aae0975a9ea2195074f9c62a3d9b9b
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-30 19:47:35 -0300

    add sort exemples

--- bloco_8/dia-2/aulas.js
+++ bloco_8/dia-2/aulas.js
@@ -54,8 +54,25 @@ const grades = {
   ingles: 'Aprovado',
 };
 
-const verifyGrades = (studentGrades) => (
-  Object.values(studentGrades).every((grade) => grade === 'Aprovado')
-);
+// const verifyGrades = (studentGrades) => (
+//   Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+// );
 
-console.log(verifyGrades(grades));
+// console.log(verifyGrades(grades));
+
+const food = ['arroz', 'feijão', 'farofa', 'chocolate', 'doce de leite'];
+food.sort();
+console.log(food);
+// [ 'arroz', 'chocolate', 'doce de leite', 'farofa', 'feijão' ]
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers.sort();
+console.log(numbers); // [1, 10, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => a - b);
+console.log(points); // [1, 5, 10, 25, 40, 100]
+
+const pointsDecreasing = [40, 100, 1, 5, 25, 10];
+pointsDecreasing.sort((a, b) => b - a);
+console.log(pointsDecreasing); // [ 100, 40, 25, 10, 5, 1 ]
