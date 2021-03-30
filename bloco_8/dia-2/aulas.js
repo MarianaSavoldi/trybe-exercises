d183a81ab94d7263402729d5aa86051b1b58f06f
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-30 19:09:11 -0300

    add for each

--- bloco_8/dia-2/aulas.js
+++ bloco_8/dia-2/aulas.js
@@ -1,9 +1,17 @@
-const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
-arrayOfValues.forEach((element) => {
-  console.log('Cada elemento do array:', element);
-});
+// const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+// arrayOfValues.forEach((element) => {
+//   console.log('Cada elemento do array:', element);
+// });
 
 // Cada elemento do array: josé
 // Cada elemento do array: 50
 // Cada elemento do array: 0.25
 // Cada elemento do array: { comida: 'Chocolate' }
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
