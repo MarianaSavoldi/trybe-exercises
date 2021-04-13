2d8f5c31e6e3e0e1b360a53a17a3c4a60e1c86ec
Mariana Savoldi <marianasavodi123@gmail.com>
2021-04-13 20:26:42 -0300

    refatorando verifiedFetch

--- bloco_9/dia-2/promises.js
+++ bloco_9/dia-2/promises.js
@@ -19,18 +19,38 @@
 // .then(msg => console.log(msg))
 // .catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
 
+// const fetch = require('node-fetch');
+
+// function verifiedFetch(url) {
+//   return new Promise((resolve, reject) => {
+//     if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+//       fetch(url)
+//         .then((r) => r.json())
+//         .then((r) => resolve(r.value));
+//     } else {
+//       reject(new Error('endpoint não existe'));
+//     }
+//   });
+// }
+
+// function sendJokeToFriend(name) {
+//   const message = verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+//     .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+//     .catch((err) => err);
+//   console.log(message);
+// }
+
+// sendJokeToFriend("Anna");
+
 const fetch = require('node-fetch');
 
-function verifiedFetch(url) {
-  return new Promise((resolve, reject) => {
-    if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
-      fetch(url)
-        .then((r) => r.json())
-        .then((r) => resolve(r.value));
-    } else {
-      reject(new Error('endpoint não existe'));
-    }
-  });
+async function verifiedFetch(url) {
+  if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => r.value);
+  }
+  throw new Error('endpoint não existe');
 }
 
 function sendJokeToFriend(name) {
